ec8d0864458e638d4a6701b7b159109e42704f65
Kelby Faessler <kmfaessler@gmail.com>
2017-06-08 21:59:32 -0500

    refactoring, still not complete

--- filecreator.py
+++ filecreator.py
@@ -27,24 +27,35 @@ def main():
     with open("./costs.csv", "r") as costs_file:
         #read costs
         costs_dict = create_costs_dict_from_file(costs_file)
-        costs_player_names = create_key_list_from_dict(costs_dict)
+        #costs_player_names = create_key_list_from_dict(costs_dict)
+        costs_player_names = sorted(costs_dict)
 
         #read birdie stats
         html = urllib2.urlopen(STATS_URL)
         #use BeautifulSoup to parse html
         soup = bsoup(html)
+
+        '''
+        print('')
+        print('')
+        print('Encoding: ' + soup.originalEncoding)
+        '''
+
         player_rows = soup.findAll('tr', {"id" : re.compile("playerStatsRow(.*)")})
         
         stats_dict = create_stats_dict_from_html(player_rows)
-        stats_player_names = create_key_list_from_dict()
+        #stats_player_names = create_key_list_from_dict(stats_dict)
+        stats_player_names = sorted(stats_dict)
+        
 
         #search csv for player name. If found, append birdie avg to csv
         #for key in costs_dict.keys():
         #   print ("key: " + key + " value: " + costs_dict[key])
-        num_matches = calculation_num_matches()
-        #print(num_matches)
+        print('')
+        num_matches = calculate_num_matches()
+        print(num_matches)
 
-        print_names_from_both_sources(costs_list, stats_list)
+        #print_names_from_both_sources(costs_list, stats_list)
         
 
 def create_costs_dict_from_file(costs_file):
@@ -80,7 +91,6 @@ def create_stats_dict_from_html(player_rows):
             #duplicate
             pass
         stats_dict[player_name] = birdie_pct_str
-        stats_list.append(player_name)
         #print(player_name)
     return stats_dict
         
@@ -99,17 +109,19 @@ def print_names_from_both_sources(costs_list, stats_list):
             print("\t\t\t" + costs_list[0])
             costs_list.pop(0)
 
-def calculation_num_matches(costs_dict, stats_dict):
+def calculate_num_matches(costs_list, stats_list):
     num_matches = 0
-    for player in costs_dict:
-        if player in stats_dict:
-
+    costs_list.sort()
+    stats_list.sort()
+    while (stats_list and costs_list):
+        if stats_list[0] == costs_list[0]:
+            num_matches += 1
+            stats_list.pop(0)
+            costs_list.pop(0)
+        elif (stats_list[0] < costs_list[0]):
+            stats_list.pop(0)
         else:
-            print(player + " not found in stats")
-        if player_name in costs_dict:
-            #print(player_name)
-            num_matches = num_matches + 1
-    return num_matches
+            costs_list.pop(0)
 
 if __name__ == "__main__":
     main()
