f8d6f90c70ad4a47633c9bd83a1beaa488d00c02
Kelby Faessler <kmfaessler@gmail.com>
2017-06-15 21:52:41 -0500

    start building main to read input stats

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 /*Compute best best ball team*/
 
+#include "Player.h"
+
+#include <iostream>
+
+//forward declarations
+void ReadPlayerStats(const char* i_fileName);
 
 int main(int argc, char* argv)
+{
+    if (argc > 1)
+    {
+        ReadPlayerStats(argv[1]);
+    }
+    else
+    {
+        std::cout << "Error: include stats file as argument" << std::endl;
+        std::cout << "Usage: BBMajorsCompute <stats .csv file>" << std::endl;
+    }
+
+}
+
+void ReadPlayerStats(const char* i_fileName)
 {
 
 }
