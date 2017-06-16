53057f85d5598026ad1ac2b4b48018add675c743
Kelby Faessler <kmfaessler@gmail.com>
2017-06-15 21:52:18 -0500

    Player data sort functions

--- Player.h
+++ Player.h
@@ -6,4 +6,42 @@ struct Player
     std::string name;
     float cost;
     float birdieAvg;
+
+    struct Player(const std::string& i_name, float i_cost, float i_birdieAvg)
+    : name(i_name)
+    , cost(i_cost)
+    , birdieAvg(i_birdieAvg)
+    {};
+};
+
+struct less_than_cost
+{
+    inline bool operator() (const Player& player1, const Player& player2)
+    {
+        return (player1.cost < player2.cost);
+    }
+};
+
+struct greater_than_cost
+{
+    inline bool operator() (const Player& player1, const Player& player2)
+    {
+        return (player1.cost > player2.cost);
+    }
+};
+
+struct less_than_birdie_avg
+{
+    inline bool operator() (const Player& player1, const Player& player2)
+    {
+        return (player1.birdieAvg < player2.birdieAvg);
+    }
+};
+
+struct greater_than_birdie_avg
+{
+    inline bool operator() (const Player& player1, const Player& player2)
+    {
+        return (player1.birdieAvg > player2.birdieAvg);
+    }
 };
