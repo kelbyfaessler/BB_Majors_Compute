ca4bdf88844e8c3169c8412ad862d072aaa9ced0
Kelby Faessler <kmfaessler@gmail.com>
2019-09-19 18:34:24 -0700

    custom user model working without saving username in db

--- Website/bbmajors_compute/users/models.py
+++ Website/bbmajors_compute/users/models.py
@@ -1,8 +1,55 @@
 from django.db import models
 #from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        # extra_fields['is_staff'] = True
+        # extra_fields['is_superuser'] = True
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+
+        return self._create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+
+    username = None
+    email=models.EmailField(_('email address'), unique=True, max_length=254, blank=False, error_messages={
+        'unique': _("A user with that email address already exists")
+    })
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
 
 """
 Custom user manager so logins happen with email instead of username.
@@ -41,12 +88,6 @@ Heavily re-used the django default UserManager
 #         is_staff = True
 #         is_superuser = True
 #         return self._create_user(email, password, is_staff, is_superuser, **extra_fields)
-
-class CustomUser(AbstractUser):
-    # Add additional custom fields here
-
-    def __str__(self):
-        return self.email
 
 # class User(AbstractBaseUser, PermissionsMixin):
 #     email=models.EmailField(_('email address'), unique=True, max_length=254, blank=False, error_messages={
